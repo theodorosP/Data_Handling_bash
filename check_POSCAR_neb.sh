b67bb40dec5461324758113697d715e9cf1d7932
thodorisP <thodorisP@github.com>
2023-12-13 05:28:19 -0500

    commit

---
+++ check_POSCAR_neb.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+for i in {0..6..1}
+do
+    a=$(diff 0$i/POSCAR ../POSCAR.$i)
+    if [ -z "$a" ];then
+        echo Same 0$i/POSCAR and POSCAR.$i
+    else
+        echo Different
+    fi
+done
+
