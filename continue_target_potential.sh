dd43e03e32dec44053771fd438a1ec3c41ae0503
thodorisP <teosfp@hotmail.com>
2024-10-11 13:08:12 -0400

    commit

--- continue_target_potential.sh
+++ continue_target_potential.sh
@@ -1,14 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
-a=`ls -ltr|grep -c ^d`
-num=$(echo "$a + 1"|bc)
-mkdir $num
-cp * $num
+a=$(ls -ltr | grep -c ^d)
+num=$(echo "$a + 1" | bc)
+mkdir "$num"
+find . -maxdepth 1 ! -name "$num" ! -name "." -exec cp -r {} "$num/" \;
 rm -rf slurm*
 mv CONTCAR POSCAR
-NELECT_OUTCAR=`grep "NELECT" OUTCAR|tail -1|awk '{print $4}'`
-echo $NELECT_OUTCAR
-NELECT_INCAR=`grep "NELECT =" INCAR|tail -1|awk '{print $3}'`
-echo $NELECT_INCAR
+NELECT_OUTCAR=$(grep "NELECT" OUTCAR | tail -1 | awk '{print $4}')
+echo "$NELECT_OUTCAR"
+NELECT_INCAR=$(grep "NELECT =" INCAR | tail -1 | awk '{print $3}')
+echo "$NELECT_INCAR"
 sed -i "s/NELECT = $NELECT_INCAR/NELECT = $NELECT_OUTCAR/g" INCAR
 sbatch job
