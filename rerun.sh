cca4b7c0264796965f86548e8accf49c88f89c8e
thodorisP <teosfp@hotmail.com>
2024-10-11 23:11:33 -0400

    commit

--- rerun.sh
+++ rerun.sh
@@ -8,10 +8,9 @@ do
     num=$(echo "$b + 1" | bc)
     mkdir "RUN_$num"
     last_folder=$(ls -1t --group-directories-first | grep '^RUN_' | head -n 1)
-    find . -maxdepth 1 ! -name "$last_folder" ! -name "." -exec cp -r {} "$last_folder/" \;
-    echo "Files Copied to to $last_folder"
+    find . -maxdepth 1 -type f ! -name "$last_folder" ! -name "." -exec cp {} "$last_folder/" \;
+    echo "Files Copied to $last_folder"
     cd ../
 done
 rm -rf slurm*
 sbatch job
-
