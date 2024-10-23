a4ce5fe1e3df06bdf74dc642816d0bbe8b463776
theodorosP <teosfp@hotmail.com>
2024-10-23 17:00:35 -0400

    cimmit

--- rerun.sh
+++ rerun.sh
@@ -3,24 +3,28 @@
 a=$(ls -d */)
 for i in $a
 do
-    cd $i
+    cd "$i"
     b=$(ls -ltr | grep -c ^d)
     num=$(echo "$b + 1" | bc)
     count=1
     for k in OUTCAR REPORT vasprun.xml XDATCAR
     do
-        if [ -f "$k" ]; then  # Check if the file exists
-            echo "Compressing $i, $count/4 files compressed: $k"
+        if [ -f "$k" ]; then
+            echo "Compressing $i$k, $count/4 files"
             gzip "$k"
-            count=$((count + 1))  # Increment the counter
+            count=$((count + 1))
         fi
     done
-    mkdir "RUN$num"
-    last_folder=$(ls -1t --group-directories-first | grep '^RUN' | head -n 1)
-    find . -maxdepth 1 -type f ! -name "OUTCAR.gz" ! -name "REPORT.gz" ! -name "vasprun.xml.gz" ! -name "XDATCAR.gz" -exec cp {} "$last_folder/" \;
+    last_folder="RUN$num"
+    mkdir "$last_folder"
+    for file in *; do
+        if [[ "$file" != "$last_folder" ]]; then
+            cp -rn "$file" "$last_folder"
+        fi
+    done
     echo "Files Copied to $last_folder"
+    rm -rf *.gz
     cd ../
 done
 rm -rf slurm*
 sbatch job
-
