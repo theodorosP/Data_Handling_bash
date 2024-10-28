47f50c3afa333fbcbd898f8091638abdbad504a8
theodorosP <teosfp@hotmail.com>
2024-10-28 15:24:06 -0400

    commit

--- rerun.sh
+++ rerun.sh
@@ -4,6 +4,19 @@ a=$(ls -d */)
 for i in $a
 do
     cd "$i"
+    echo Folder: $i
+    ./rerun.sh
+    cd ../
+done
+[trahman@viper02 MD]$ cd -
+/viper/u/trahman/data/theo/HER_Au/MD/CH3NH3
+[trahman@viper02 CH3NH3]$ cat rerun.sh 
+#!/bin/bash
+
+a=$(ls -d */)
+for i in $a
+do
+    cd "$i"
     b=$(ls -ltr | grep -c ^d)
     num=$(echo "$b + 1" | bc)
     count=1
@@ -16,13 +29,15 @@ do
         fi
     done
     last_folder="RUN$num"
+    files=`ls -p | grep -v /`
     mkdir "$last_folder"
-    for file in *; do
+    for file in $files; do
         if [[ "$file" != "$last_folder" ]]; then
             cp -rn "$file" "$last_folder"
         fi
     done
     echo "Files Copied to $last_folder"
+    mv CONTCAR POSCAR
     rm -rf *.gz
     cd ../
 done
