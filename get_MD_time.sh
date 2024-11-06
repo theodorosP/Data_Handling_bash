56347ded0e6b815395673562d4044ca70ea1b0a6
theodorosP <teosfp@hotmail.com>
2024-11-06 10:22:02 -0500

    commit

--- get_MD_time.sh
+++ get_MD_time.sh
@@ -7,14 +7,22 @@ for i in $dirs
     echo " "
     echo "dir = $i"
     cd "$i"
-    current_time=$(grep Iter OUTCAR | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
-    echo "current_time = $current_time"
+    if [ -f "OUTCAR" ]; then
+        current_time=$(grep Iter OUTCAR | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
+        echo "current_time = $current_time"
+    elif [ -f "OUTCAR.gz" ]; then
+        current_time=$(zgrep Iter OUTCAR.gz | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
+        echo "current_time = $current_time"
+    else
+        echo "NO OUTCAR  or OUTCAR.gz"
+    fi
     for dir in RUN*/
         do
         if [ -d "$dir" ]; then
             cd "$dir"
-            echo "Directory: $i/$dir"
+            #echo "Directory: $i/$dir"
             new_time=$(zgrep Iter OUTCAR.gz | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
+            echo "$dir time: $new_time"
             current_time=$((current_time + new_time))
             cd ../
         fi
