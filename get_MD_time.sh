cd307a6019c90867d8487b1630f18053dc204507
thodorisP <teosfp@hotmail.com>
2024-11-14 10:48:52 -0500

    commit

--- get_MD_time.sh
+++ get_MD_time.sh
@@ -11,38 +11,26 @@ do
     if [ -f "OUTCAR" ]; then
         current_time=$(grep -a Iter OUTCAR | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
         echo "current_time = $current_time"
-    elif [ -f "OUTCAR.gz" ]; then
-        if file OUTCAR.gz | grep -q "gzip compressed data"; then
-            current_time=$(zcat OUTCAR.gz | strings | grep -n Iter | tail -1 | sed 's/.*Iteration *\([0-9]*\).*/\1/')
-            echo "current_time (binary) = $current_time"
-        else
-            current_time=$(zgrep Iter OUTCAR.gz | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
-            echo "current_time (text) = $current_time"
-        fi
     else
         echo "NO OUTCAR or OUTCAR.gz"
     fi
-
     for dir in RUN*/
     do
         if [ -d "$dir" ]; then
             cd "$dir"
-            if file OUTCAR.gz | grep -q "gzip compressed data"; then
-                # If OUTCAR.gz is binary, use zcat, strings
-                new_time=$(zcat OUTCAR.gz | strings | grep -n Iter | tail -1 | sed 's/.*Iteration *\([0-9]*\).*/\1/')
-                echo "$dir time (binary): $new_time"
-            else
-                # If OUTCAR.gz is not binary
-                new_time=$(zgrep Iter OUTCAR.gz | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
-                echo "$dir time (text): $new_time"
-            fi
-            current_time=$((current_time + new_time))  # Add the time from this RUN subdirectory
+            new_time=$(zcat OUTCAR.gz | strings | grep -n Iter | tail -1 | sed 's/.*Iteration *\([0-9]*\).*/\1/')
+            echo "$dir time: $new_time"
+            current_time=$((current_time + new_time))
             cd ../
         fi
     done
-
     echo "For $i, total RunTime is: $((current_time / 1000)) ps"
     echo " "
-
     cd ../
 done
+
+for i in {1..20};
+do
+    echo -n "--"
+done
+echo " "
