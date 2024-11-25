e48b700e9d2f9ca60f99cff650ec973f6fee294e
thodorisP <teosfp@hotmail.com>
2024-11-25 06:44:30 -0500

    commit

--- get_MD_time.sh
+++ get_MD_time.sh
@@ -6,21 +6,17 @@ for i in $dirs
 do
     echo " "
     echo "dir = $i"
-
     if cd "$i"; then
         current_time=0
-
         if [ -f "OUTCAR" ]; then
             current_time=$(grep -a Iter OUTCAR | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
             echo "current_time = $current_time"
         else
             echo "NO OUTCAR or OUTCAR.gz"
         fi
-
         for dir in $(ls -d RUN*/ | sort -V)
         do
             if [ -d "$dir" ]; then
-                # Change to the subdirectory; skip processing if it fails
                 if cd "$dir"; then
                     if [ -f "OUTCAR.gz" ]; then
                         new_time=$(zcat OUTCAR.gz | strings | grep -n Iter | tail -1 | sed 's/.*Iteration *\([0-9]*\).*/\1/')
@@ -33,13 +29,11 @@ do
                 fi
             fi
         done
-
         echo "For $i, total RunTime is: $((current_time / 1000)) ps"
         cd ../
     fi
 done
 
-# Print a separator line
 for i in {1..20};
 do
     echo -n "--"
