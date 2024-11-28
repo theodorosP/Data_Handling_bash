ea00cc17f20307aaafa3325c0f9ceac3f6cde443
thodorisP <teosfp@hotmail.com>
2024-11-28 17:34:59 -0500

    commit

--- continue_target_potential.sh
+++ continue_target_potential.sh
@@ -2,11 +2,11 @@
 
 a=$(ls -ltr | grep -c ^d)
 num=$(echo "$a + 1" | bc)
-mkdir "$num"
-find . -maxdepth 1 ! -name "$num" ! -name "." -exec cp -r {} "$num/" \;
+mkdir "RUN$num"
+find . -maxdepth 1 -type f -exec cp {} "RUN$num/" \;
 rm -rf slurm*
 mv CONTCAR POSCAR
-NELECT_OUTCAR=$(grep "NELECT" OUTCAR | tail -1 | awk '{print $4}')
+NELECT_OUTCAR=$(grep "NELECTCURRENT" OUTCAR | tail -1 | awk '{print $2}')
 echo "$NELECT_OUTCAR"
 NELECT_INCAR=$(grep "NELECT =" INCAR | tail -1 | awk '{print $3}')
 echo "$NELECT_INCAR"
