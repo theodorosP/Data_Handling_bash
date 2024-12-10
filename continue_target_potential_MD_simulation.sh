e15675aeb3c1b7cc1963254bc5d6a895998e5974
thodorisP <teosfp@hotmail.com>
2024-12-10 08:40:55 -0500

    commit

--- continue_target_potential_MD_simulation.sh
+++ continue_target_potential_MD_simulation.sh
@@ -1,29 +1,28 @@
-k#!/bin/bash
+#!/bin/bash
 
 cont(){
-a=$(ls -ltr | grep -c ^d)
-num=$(echo "$a + 1" | bc)
-mkdir "RUN$num"
 if [[ -f OUTCAR ]]; then
+    a=$(ls -ltr | grep -c ^d)
+    num=$(echo "$a + 1" | bc)
+    mkdir "RUN$num"
     echo Zipping OUTCAR
     gzip OUTCAR
+    find . -maxdepth 1 -type f -exec cp {} "RUN$num/" \;
+    rm -rf slurm*
+    mv CONTCAR POSCAR
+    NELECT_OUTCAR=$(zcat OUTCAR.gz | strings | grep "NELECTCURRENT" | tail -1 | awk '{print $2}')
+    echo "$NELECT_OUTCAR"
+    NELECT_INCAR=$(grep "NELECT =" INCAR | tail -1 | awk '{print $3}')
+    echo "$NELECT_INCAR"
+    sed -i "s/NELECT = $NELECT_INCAR/NELECT = $NELECT_OUTCAR/g" INCAR
+    rm -rf OUTCAR.gz
 else
     echo -e "\e[5mOUTCAR NOT found. Moving to the next folder.\e[0m"
 	return 1
 fi
-find . -maxdepth 1 -type f -exec cp {} "RUN$num/" \;
-rm -rf slurm*
-mv CONTCAR POSCAR
-NELECT_OUTCAR=$(zgrep "NELECTCURRENT" OUTCAR.gz | tail -1 | awk '{print $2}')
-echo "$NELECT_OUTCAR"
-NELECT_INCAR=$(grep "NELECT =" INCAR | tail -1 | awk '{print $3}')
-echo "$NELECT_INCAR"
-sed -i "s/NELECT = $NELECT_INCAR/NELECT = $NELECT_OUTCAR/g" INCAR
-rm -rf OUTCAR.gz
 }
 
 
-cd /viper/u/trahman/data/theo/HER_Au/slow_grow_method/NH4/5_NH4
 dirs=$(ls -d */ | sort -V )
 
 for i in $dirs
@@ -33,4 +32,3 @@ do
     cont
     cd ../
 done
-
