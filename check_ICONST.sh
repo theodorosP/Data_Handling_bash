589dd4ac6fc3339681cf04db5271d33c76d7430f
thodorisP <teosfp@hotmail.com>
2024-12-22 09:27:28 -0500

    commit

--- check_ICONST.sh
+++ check_ICONST.sh
@@ -5,22 +5,19 @@ for dir1 in "$ROOT_DIR"/*/; do
 	if [[ ! -f "$dir1/ICONST" ]]; then
 		continue
 	fi
-
-    ICONST_CONTENT=$(<"$dir1/ICONST")
-
-    for dir2 in "$ROOT_DIR"/*/; do
-        if [[ "$dir1" == "$dir2" ]]; then
-            continue
-        fi
-
-        if [[ -f "$dir2/ICONST" ]]; then
-            OTHER_ICONST_CONTENT=$(<"$dir2/ICONST")
-            if [[ "$ICONST_CONTENT" == "$OTHER_ICONST_CONTENT" ]]; then
-                echo "Duplicate ICONST found:"
-                echo "$dir1/ICONST"
-                echo "$dir2/ICONST"
+	ICONST_CONTENT=$(<"$dir1/ICONST")
+	for dir2 in "$ROOT_DIR"/*/; do
+		if [[ "$dir1" == "$dir2" ]]; then
+			continue
+		fi
+		if [[ -f "$dir2/ICONST" ]]; then
+			OTHER_ICONST_CONTENT=$(<"$dir2/ICONST")
+			if [[ "$ICONST_CONTENT" == "$OTHER_ICONST_CONTENT" ]]; then
+				echo "Duplicate ICONST found:"
+				echo "$dir1/ICONST"
+				echo "$dir2/ICONST"
 				echo ""
-            fi
-        fi
-    done
+			fi
+		fi
+	done
 done
