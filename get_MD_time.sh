f06e7b8d38f0efd9f6c6e35824e8de7b73229c53
theodorosP <teosfp@hotmail.com>
2025-01-21 13:00:01 -0500

    commit

--- get_MD_time.sh
+++ get_MD_time.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 
-dirs=$(ls -d */ | sort -V)
+dirs=$(ls -d */)
 
 for i in $dirs
-do
-	echo " "
-	echo "dir = $i"
-	if cd "$i"; then
-		current_time=0
-		if [ -f "OUTCAR" ]; then
-			current_time=$(grep -a Iter OUTCAR | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
-			echo "current_time = $current_time"
-		else
-			echo "NO OUTCAR or OUTCAR.gz"
-		fi
-		for dir in $(ls -d RUN*/ | sort -V)
-		do
-			if [ -d "$dir" ]; then
-				if cd "$dir"; then
-					if [ -f "OUTCAR.gz" ]; then
-						new_time=$(zcat OUTCAR.gz | strings | grep -n Iter | tail -1 | sed 's/.*Iteration *\([0-9]*\).*/\1/')
-						echo "$dir time: $new_time"
-						current_time=$((current_time + new_time))
-					else
-						echo "NO OUTCAR.gz in $dir"
-					fi
-					cd ../
-				fi
-			fi
-		done
-		echo "For $i, total RunTime is: $((current_time / 1000)) ps"
-		cd ../
-	fi
+    do
+    echo " "
+    echo "dir = $i"
+    cd "$i"
+    if [ -f "OUTCAR" ]; then
+        current_time=$(grep Iter OUTCAR | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
+        echo "current_time = $current_time"
+    elif [ -f "OUTCAR.gz" ]; then
+        current_time=$(zgrep Iter OUTCAR.gz | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
+        echo "current_time = $current_time"
+    else
+        echo "NO OUTCAR or OUTCAR.gz"
+        current_time=0
+    fi
+    for dir in RUN*/
+        do
+        if [ -d "$dir" ]; then
+            cd "$dir"
+            if [ -f "OUTCAR.gz" ]; then
+                new_time=$(zgrep Iter OUTCAR.gz | tail -1 | awk '{gsub(/\(.*\)/, ""); print $3}')
+                new_time=${new_time:-0} # Default to 0 if new_time is unset
+                echo "$dir time: $new_time"
+                current_time=$((current_time + new_time))
+            else
+                echo "$dir has no OUTCAR.gz"
+                new_time=0
+            fi
+            cd ../
+        fi
+    done
+    echo "For $i, total RunTime is: $((current_time / 1000)) ps"
+    echo " "
+    cd ../
 done
-
-for i in {1..20};
-do
-    echo -n "--"
-done
-echo " "
